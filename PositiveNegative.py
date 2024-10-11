15dc42168a3b4712d0d7dceb939b431e6081397e
Sabique-Islam <sabiqueislam7@gmail.com>
2024-10-11 12:12:17 +0530

    Negative integer output issue fixed

--- PositiveNegative.py
+++ PositiveNegative.py
@@ -2,7 +2,7 @@
 
 num = input("Enter a number: ")
 
-if num.isdigit():
+if num.lstrip('-').isdigit(): #lstrip() removes the '-' (if num is a negative integer) before isdigit() is called, because isdigit() only checks for non-negative integers.
     num = int(num)
     if num>0:
         print("The number entered is positive.")
@@ -15,3 +15,4 @@ else:
 
 # Issues: - 
 # When input is a negative integer, else statement is executed along with it.
+# Issue is fixed :)
